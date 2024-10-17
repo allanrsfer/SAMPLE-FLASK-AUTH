f92e963a27f784e0ee001d1e3a4e79fc790d51d4
Allan Rieri <allanrieri@gmail.com>
2024-10-17 15:11:13 -0300

    added some imports from fask_login

--- login_manager.py
+++ login_manager.py
@@ -1,3 +1,9 @@
-from flask_login import LoginManager
+from flask_login import (
+    LoginManager,
+    login_user,
+    current_user,
+    logout_user,
+    login_required,
+)
 
 login_manager = LoginManager()
