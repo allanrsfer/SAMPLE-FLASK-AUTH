3be798b84d53e6ad54e5130d286720f02ae32ac1
Allan Rieri <allanrieri@gmail.com>
2024-10-17 10:01:11 -0300

    Created login_manager.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from models.user import User
 from database import db
+from login_manager import login_manager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
+
 db.init_app(app)
+login_manager.init_app(app)
 
 
 @app.route("/", methods=["GET"])
